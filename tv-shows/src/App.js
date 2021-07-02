31782711ccd8ca00c7df7f627feccc446e4af8cb
Nicos Tsourektsidis <creatorgr@hotmail.com>
2021-07-02 10:40:45 +0200

    Commit popular shows

--- tv-shows/src/App.js
+++ tv-shows/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import popularShows from './popular-shows.json';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-      </header>
+    <div>
+      <h1>TV Shows</h1>
+
+      <ul>
+        {popularShows.results.map(({ name, vote_average }) => (
+          <li>
+            {name}, Rating: {vote_average}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
