08f26ba4177f77b47ee11d1b754553586aee246d
Mihaly Verhas <Mihaly_Verhas@epam.com>
2021-07-03 15:22:44 +0200

    Added fetch

--- tv-shows/src/screens/DetailsScreen/DetailsScreen.jsx
+++ tv-shows/src/screens/DetailsScreen/DetailsScreen.jsx
@@ -1,16 +1,39 @@
-import {
-    useParams
-} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Breadcrumb } from "semantic-ui-react";
 
 const DetailsScreen = () => {
+  const [details, setDetails] = useState({});
+  let { id } = useParams();
 
-    let { id } = useParams();
+  useEffect(() => {
+    const params = new URLSearchParams({
+      api_key: process.env.REACT_APP_API_KEY,
+      language: "en-US",
+    });
+    fetch(`https://api.themoviedb.org/3/tv/${id}?${params}`)
+      .then((data) => data.json())
+      .then((data) => {
+        const { genres, name, overview } = data;
+        setDetails({ genres, name, overview });
+      })
+      .catch((error) => console.error(error));
+  }, [id, setDetails]);
 
-    return (
-        <div>
-            Check if it works {id}
-        </div>
-    )
-}
+  return (
+    <div>
+      <Breadcrumb>
+        <Breadcrumb.Section link>Home</Breadcrumb.Section>
+        <Breadcrumb.Divider />
+        <Breadcrumb.Section active>Details</Breadcrumb.Section>
+      </Breadcrumb>
+      <ul>
+        <li>{details.name}</li>
+        <li>{details.genres}</li>
+        <li>{details.overview}</li>
+      </ul>
+    </div>
+  );
+};
 
 export default DetailsScreen;
